25e622c56cbdd21b93bed902ae9e7f234d07f62a
Jaime Conde <jaime.conde.baruque@gmail.com>
2025-01-15 21:03:20 +0100

    Periodic Table
    script and dbc from the periodic table excercise

---
+++ relational-databases/periodic-table-database/element.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+FORMAT_STRING() {
+  echo "$(echo $1 | sed -E 's/^ *| *\+$//g')"
+}
+
+# Selector de elemento
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+else
+  # Checkeamos qué tipo es el input
+  if [[ "$1" =~ ^[0-9]+$ ]]; then
+    # Atomic_number
+    FILTER="atomic_number = $1"
+  else
+    if [[ $(echo "$1" | wc -c) -gt 3 ]]; then
+        # Name
+        FILTER="name = '$1'"
+    else
+        # Symbol
+        FILTER="symbol = '$1'"
+    fi
+  fi
+  # Cogemos el elemento de la tabla
+  CHECK_ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE $FILTER")
+  # Checkeamos si no está vacio
+  if [[ -z $CHECK_ELEMENT ]]; then
+    echo "I could not find that element in the database."
+  else
+    IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< $CHECK_ELEMENT+
+    AVER= echo "The element with atomic number $(FORMAT_STRING $ATOMIC_NUMBER) is $(FORMAT_STRING $NAME) ($(FORMAT_STRING $SYMBOL)). It's a $(FORMAT_STRING $TYPE), with a mass of $(FORMAT_STRING $ATOMIC_MASS) amu. $(FORMAT_STRING $NAME) has a melting point of $(FORMAT_STRING $MELTING_POINT) celsius and a boiling point of $(FORMAT_STRING $BOILING_POINT) celsius."
+  fi
+fi
+
+
